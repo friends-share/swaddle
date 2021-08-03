3614daec7d3faa82c40eb39ccc5a980d09a366c9
Manu <grajmanu@gmail.com>
2021-08-03 20:54:22 +0530

    Fix incomplete method error

--- src/deploying_plugin/stack_deployer.py
+++ src/deploying_plugin/stack_deployer.py
@@ -47,14 +47,8 @@ class StackDeployer(Deploying):
                     Command(command=f"git clone {app.git.repo} ."),
                     Command(command=f"echo docker-compose -f {app.docker_file} build >{deployment_id}.sh", privileged=privileged),
                     Command(command=f"echo docker stack deploy -c {app.docker_file} {app.name} >>{deployment_id}.sh", privileged=privileged),
-                    Command(
-                        command=f"echo 'status=$?'>>{deployment_id}.sh",
-                        privileged=privileged),
-                    Command(command=f"echo 'if [[ \"$status\" == \"0\" ]]; then touch {deployment_id}.success fi'>>{deployment_id}.sh", privileged=privileged),
-                    Command(
-                        command=f"echo 'if [[ \"$status\" != \"0\" ]]; then touch {deployment_id}.failed fi'>>{deployment_id}.sh",
-                        privileged=privileged),
-
+                    Command(command=f"echo 'status=$?'>>{deployment_id}.sh",  privileged=privileged),
+                    Command(command=f"touch {deployment_id}.$?'>>{deployment_id}.sh", privileged=privileged),
                     Command(command=f"chmod +x {deployment_id}.sh", privileged=privileged),
                     Command(command=f"nohup ./{deployment_id}.sh &", privileged=privileged)
                 ])
