768617543952c46e33247f19f118b6d10fc5da10
Manu <grajmanu@gmail.com>
2021-08-03 19:27:31 +0530

    Add timeout optimisation

--- src/core/ssh2.py
+++ src/core/ssh2.py
@@ -30,6 +30,8 @@ class SSHClient2:
 
     def run_all(self, commands: List[Command]):
         def execute_commands(job, host, conn: SSH2):
+            conn.autoinit()
+            conn.set_timeout(3600)
             for command in commands:
                 conn.execute("sudo " + command.command if command.privileged else command.command)
                 conn.execute("echo $?")
