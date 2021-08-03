ce8bab7d65deaf99c379dc35b082569292d30d06
Manu <grajmanu@gmail.com>
2021-08-03 21:13:15 +0530

    Fix dangling comma

--- src/deploying_plugin/stack_deployer.py
+++ src/deploying_plugin/stack_deployer.py
@@ -47,7 +47,7 @@ class StackDeployer(Deploying):
                     Command(command=f"git clone {app.git.repo} ."),
                     Command(command=f"echo docker-compose -f {app.docker_file} build >{deployment_id}.sh", privileged=privileged),
                     Command(command=f"echo docker stack deploy -c {app.docker_file} {app.name} >>{deployment_id}.sh", privileged=privileged),
-                    Command(command=f"echo touch {deployment_id}.\"$?\"'>>{deployment_id}.sh", privileged=privileged),
+                    Command(command=f"echo touch {deployment_id}.\"$?\">>{deployment_id}.sh", privileged=privileged),
                     Command(command=f"chmod +x {deployment_id}.sh", privileged=privileged),
                     Command(command=f"nohup ./{deployment_id}.sh &", privileged=privileged)
                 ])
