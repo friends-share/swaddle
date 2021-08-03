76cb84012e3c4fffff456eda40df2ba84980903a
Manu <grajmanu@gmail.com>
2021-08-03 19:57:35 +0530

    Try keepalive and reconnect

--- src/core/ssh2.py
+++ src/core/ssh2.py
@@ -31,6 +31,7 @@ class SSHClient2:
     def run_all(self, commands: List[Command]):
         def execute_commands(job, host, conn: SSH2):
             conn.KEEPALIVE_INTERVAL = 3600
+            conn.client = conn._paramiko_connect()
             conn.autoinit()
             conn.set_timeout(3600)
             conn.set_connect_timeout(3600)
