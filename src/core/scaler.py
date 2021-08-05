b39ba9ca806931dd1f619cab3fd0e7fb2c89931e
Manu <grajmanu@gmail.com>
2021-08-05 20:58:13 +0530

    Add debug configuration

--- src/core/scaler.py
+++ src/core/scaler.py
@@ -7,25 +7,27 @@ group_manager = Manager.GROUPED_DATA_MANAGER
 
 def scale(group: str, app_name: str, scale: int):
     app_log = group_manager.get_app_log(group, app_name)
-    command = f"docker stack services {app_name} - -format = '{{.Name}}={{.Replicas}}'"
+    command = "docker stack services "+app_name+" --format = '{{.Name}}={{.Replicas}}'"
     cluster_ids = list(app_log.deployments.keys())
     data = []
     for cluster_id in cluster_ids:
         cluster = group_manager.get_cluster(group, cluster_id)
         manager = cluster.data.managers[0]
         out = ",".join(SSH.connect_server(manager).run(Command(command=command, privileged=manager.privileged)).out)
+        print(out)
         data.append({cluster_id: out})
     return data
 
 
 def get_scale(group: str, app_name: str):
     app_log = group_manager.get_app_log(group, app_name)
-    command = f"docker stack services {app_name} - -format = '{{.Name}}={{.Replicas}}'"
+    command = "docker stack services " + app_name + " --format = '{{.Name}}={{.Replicas}}'"
     cluster_ids = list(app_log.deployments.keys())
     data = []
     for cluster_id in cluster_ids:
         cluster = group_manager.get_cluster(group, cluster_id)
         manager = cluster.data.managers[0]
         out = ",".join(SSH.connect_server(manager).run(Command(command=command, privileged=manager.privileged)).out)
+        print(out)
         data.append({cluster_id: out})
     return data
